5b701a21a94fa84f4df793b7d87d0ac544808d1e
Berkeley V-H <berkeleyvh20@gmail.com>
2022-03-15 22:22:51 +0000

    Winning works

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import './Board.css';
 //Start out with a board, this will contain the order of the array
 //Place nine squares in order, separated by rows of className
-//Create square array
+//What happens when a square is clicked onClick
+//The starting value is null, 
 
 type Props = {
   handleClick: () => void;
@@ -11,7 +12,7 @@ type Props = {
 
 const Square = ({handleClick, value}: Props) => {
 
-  return <button>
+  return <button onClick={handleClick}>
           {value}
         </button>
 }
@@ -19,17 +20,62 @@ const Square = ({handleClick, value}: Props) => {
 const Board = () => {
   const [squares, setSquares] = React.useState(Array(9).fill(null))
   const [isX, setIsX] = React.useState(true)
+  const [winner, setWinner] = React.useState<null | String>(null)
+  let [status, setStatus] = React.useState<String>('')
+  const handleClick = (i:number) => {
+    if (squares[i] || (winner !== null)){
+      return
+    }
+    squares[i] = isX ? 'X' : 'O'
+    setSquares(squares)
+    setIsX(!isX)    
+    setWinner(calculateWinner(squares))
+  }
+  
+  const resetBoard = () => {
+    setSquares(Array(9).fill(null))
+    setIsX(true)
+    setWinner(null)
+  }
 
-  const handleClick = () => {
-    
-
+  const renderSquare = (i:number) => {
+    return <Square handleClick={() => handleClick(i)} value={squares[i]}/>
   }
 
   return (
-    <>
-      <Square handleClick={handleClick} value={squares[0]}/>
-    </>
-  );
+    <div className="wrapper">
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
+      <button onClick={resetBoard}>Reset</button>
+    </div>
+  )
 }
 
-export default Board;
+const calculateWinner = (arr: Array<String | null>) => {
+  const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [6,4,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+  ]
+  for(let i=0; i<winningCombinations.length; i++){
+    const [a, b, c] = winningCombinations[i];
+    if(arr[a] && arr[a] === arr[b] && arr[a] === arr[c]){
+      return arr[a]
+    }
+  }
+  return null
+}
+
+export default Board
